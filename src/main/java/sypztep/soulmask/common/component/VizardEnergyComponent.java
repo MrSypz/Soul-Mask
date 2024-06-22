dff8ac3da7622232c63cfa70a19a2bb24757575f
chai <chainot@hotmail.co.th>
2024-06-23 01:37:25 +0700

    Figure out it should only client side :)

--- src/main/java/sypztep/soulmask/common/component/VizardEnergyComponent.java
+++ src/main/java/sypztep/soulmask/common/component/VizardEnergyComponent.java
@@ -8,7 +8,6 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent;
 import org.ladysnake.cca.api.v3.component.tick.CommonTickingComponent;
 import sypztep.soulmask.common.init.ModEntityComponents;
-import sypztep.soulmask.common.payload.EnergyPayload;
 import sypztep.soulmask.common.util.VizardComponentUtil;
 
 public class VizardEnergyComponent implements AutoSyncedComponent, CommonTickingComponent {
@@ -29,24 +28,9 @@ public class VizardEnergyComponent implements AutoSyncedComponent, CommonTicking
         tag.putInt("energy", this.energy);
     }
 
-    @Override
-    public void applySyncPacket(RegistryByteBuf buf) {
-        this.energy = buf.readVarInt();
-    }
-
-    @Override
-    public void writeSyncPacket(RegistryByteBuf buf, ServerPlayerEntity recipient) {
-        buf.writeVarInt(this.energy);
-    }
-    @Override
-    public boolean shouldSyncWith(ServerPlayerEntity player) {
-        return player == this.obj;
-    }
-
     public static VizardEnergyComponent getComponent(PlayerEntity player) {
         return ModEntityComponents.VIZARD_ENERGY.get(player);
     }
-
     public int getEnergy() {
         return energy;
     }
@@ -55,25 +39,26 @@ public class VizardEnergyComponent implements AutoSyncedComponent, CommonTicking
         this.energy = energy;
         sync();
     }
-    @Override
-    public void serverTick() {
-//        drainNRegenEnergy();
-//        System.out.println("Server: " + VizardEnergyComponent.getComponent(obj).getEnergy());
-        tick();
-    }
 
+    /**
+     * Energy it gonna only handle on client side why?
+     * cuz when handle with server it fuck up
+     */
     @Override
     public void clientTick() {
-        if (VizardComponentUtil.isHasEquipMask(obj))
-            EnergyPayload.send((byte) 0);
-         else
-             EnergyPayload.send((byte) 1);
+        if (VizardComponentUtil.isHasEquipMask(obj) && this.energy > 0)
+            setEnergy(this.energy - 1);
+        else if (this.energy < 100) {
+            setEnergy(this.energy + 1);
+        }
+//        System.out.println("Client: " + obj.getName() + "- " + VizardEnergyComponent.getComponent(obj).getEnergy());
         tick();
     }
 
     @Override
     public void tick() {
     }
+
     public void sync() {
         ModEntityComponents.VIZARD_ENERGY.sync(this.obj);
     }
