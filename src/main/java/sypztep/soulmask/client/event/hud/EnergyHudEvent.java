668b429e52446897d142b40c6c6a52a5531d9c93
chai <chainot@hotmail.co.th>
2024-06-23 14:38:27 +0700

    Did a Thing tho

--- src/main/java/sypztep/soulmask/client/event/hud/EnergyHudEvent.java
+++ src/main/java/sypztep/soulmask/client/event/hud/EnergyHudEvent.java
@@ -13,19 +13,17 @@ import sypztep.soulmask.common.util.VizardComponentUtil;
 
 public class EnergyHudEvent implements HudRenderCallback {
     private static final Identifier ENERGY = SoulMaskMod.id("textures/gui/mask.png");
-
     @Override
     public void onHudRender(DrawContext drawContext, RenderTickCounter tickCounter) {
         MinecraftClient minecraft = MinecraftClient.getInstance();
         int windowWidth = drawContext.getScaledWindowWidth();
         int windowHeight = drawContext.getScaledWindowHeight();
         ModEntityComponents.VIZARD_ENERGY.maybeGet(MinecraftClient.getInstance().getCameraEntity()).ifPresent(vizardComponent -> {
-            if (vizardComponent.getEnergy() <= 100) {
+            if (vizardComponent.getEnergy() <= 100)
                 renderCooldownBar(drawContext, windowWidth / 2 - 16, windowHeight / 2 + 40, vizardComponent.getEnergy(), 100);
-            }
         });
         if (VizardComponentUtil.canUseMask(minecraft.player)) {
-            if (EquipMaskEvent.getKeypressed() < 20 )
+            if (EquipMaskEvent.getKeypressed() < 20)
                 renderCooldownBar(drawContext, windowWidth / 2 - 16, windowHeight / 2 + 18, EquipMaskEvent.getKeypressed(), EquipMaskEvent.getDefaultCooldown());
         }
     }
