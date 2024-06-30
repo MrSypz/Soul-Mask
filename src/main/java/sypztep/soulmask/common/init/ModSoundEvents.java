fac06606c41bad64131dbb32b438a6e8365f5664
chai <chainot@hotmail.co.th>
2024-06-30 19:34:26 +0700

    revert

--- src/main/java/sypztep/soulmask/common/init/ModSoundEvents.java
+++ src/main/java/sypztep/soulmask/common/init/ModSoundEvents.java
@@ -2,23 +2,25 @@ package sypztep.soulmask.common.init;
 
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import sypztep.soulmask.SoulMaskMod;
 
-public class ModSoundEvents {
-    public static RegistryEntry.Reference<SoundEvent> ENTITY_GENERIC_CHARGE2;
-    public static void init() {
-        ENTITY_GENERIC_CHARGE2 = registerReference("entity.generic.charge2");
-    }
-    private static RegistryEntry.Reference<SoundEvent> registerReference(String id) {
-        return registerReference(Identifier.ofVanilla(id));
-    }
-    private static RegistryEntry.Reference<SoundEvent> registerReference(Identifier id) {
-        return registerReference(id, id);
-    }
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-    private static RegistryEntry.Reference<SoundEvent> registerReference(Identifier id, Identifier soundId) {
-        return Registry.registerReference(Registries.SOUND_EVENT, id, SoundEvent.of(soundId));
+public interface ModSoundEvents {
+    Map<SoundEvent, Identifier> SOUND_EVENTS = new LinkedHashMap();
+    //Sound
+    SoundEvent ENTITY_GENERIC_CHARGE2 = createSoundEvent("entity.generic.charge2");
+    static void init() {
+        SOUND_EVENTS.keySet().forEach((soundEvent) -> {
+            Registry.register(Registries.SOUND_EVENT, SOUND_EVENTS.get(soundEvent), soundEvent);
+        });
+    }
+    private static SoundEvent createSoundEvent(String path) {
+        SoundEvent soundEvent = SoundEvent.of(SoulMaskMod.id(path));
+        SOUND_EVENTS.put(soundEvent, SoulMaskMod.id(path));
+        return soundEvent;
     }
 }
